381f3bcf678f2c0c91ba04ae24cabf9fe0145dec
Laura Robertson <mslaura.mae@gmail.com>
2017-11-14 15:27:19 -0800

    Added scorechart constant, pseudocode scoring

--- scrabble.js
+++ scrabble.js
@@ -1,6 +1,22 @@
+/* eslint-disable */
+
 const Scrabble = {
   score: function(word) {
-    // TODO: implement score
+    const scoreWord = word.toLowerCase().split('');
+    console.log(scoreWord);
+
+    const SCORECHART = {
+      a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8,
+      k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1,
+      u: 1, v: 4, w: 4, x: 8, y: 4, z: 10,
+    };
+
+    let score = 0;
+    scoreWord.forEach(function (letter) {
+      // compare to SCORECHART
+      // add to score variable
+    });
+    // return score variable
   }
 
   // TODO: add the highestScoreFrom method
@@ -12,3 +28,5 @@ Scrabble.Player = class {
 };
 
 module.exports = Scrabble;
+
+Scrabble.score('WORD');
