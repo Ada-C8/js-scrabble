0c5ba01a86bfbf625249bfe1a9ce72d2d7311586
Tanja Stroble <plusdeltah@gmail.com>
2017-11-14 15:55:39 -0800

    adds score

--- scrabble.js
+++ scrabble.js
@@ -1,12 +1,31 @@
 
 const Scrabble = {
+  let letterScores = {
+      A: 1, E: 1, I: 1, O: 1, U: 1,
+      L: 1, N: 1, R: 1, S: 1, T: 1,
+      D: 2, G: 2, B: 3, C: 3, M: 3,
+      P: 3, F: 4, H: 4, V: 4, W: 4,
+      Y: 4, K: 5, J: 8, X: 8, Q: 10,
+      Z: 10
+    };
+
   score: function(word) {
-    // TODO: implement score
+
+    let letters = word.toUpperCase().split("");
+    let wordScore = 0;
+
+    for (let letter in letters){
+      if (letterScores[letter] !== null) {
+        wordScore += letterScores[letter];
+      }
+      if word.length === 7{
+        wordScore += 50;
+      }
+    }
+    return wordScore;
   }
 
   // TODO: add the highestScoreFrom method
-
-};
 
 Scrabble.Player = class {
   // TODO: implement the Player class
