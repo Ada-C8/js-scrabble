1ea4b1c5583b4b8ae5b30ea084a76afe696139cf
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-14 15:23:24 -0800

    implement Scrabble#score

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,46 @@
 const Scrabble = {
-  score: function(word) {
-    // TODO: implement score
-  }
+  VALUES: {
+    A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5,
+    L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4,
+    W: 4, X: 8, Y: 4, Z: 10
+  },
+  wordCheck: /\W|_|\d/,
 
+  score: function score(word) {
+    let wordScore = 0;
+
+    if (Scrabble.wordCheck.test(word)) {
+      throw new Error('Input contains invalid characters');
+    }
+    word = word.toUpperCase();
+
+    if (word.length === 7) {
+      wordScore += 50;
+    } else if (word.length > 7) {
+      throw new Error('Input is too long - cannot exceed 7 letters');
+    } else if (word.length === 0) {
+      throw new Error('Input cannot be empty');
+    }
+
+    for (let letter of word) {
+      wordScore += Scrabble.VALUES[letter];
+    }
+    return wordScore;
+  },
+
+  highestScoreFrom: function highestScoreFrom(words) {
+    let highestWord = words[0];
+    let bestScore = 0;
+    let currentScore = 0;
+    for (let word of words) {
+      currentScore = Scrabble.score(word);
+      if (currentScore > bestScore) {
+        highestWord = word;
+        bestScore = currentScore;
+      }
+    }
+    return highestWord;
+  }
   // TODO: add the highestScoreFrom method
 
 };
@@ -12,3 +50,5 @@ Scrabble.Player = class {
 };
 
 module.exports = Scrabble;
+
+console.log(Scrabble.score('dog'));
