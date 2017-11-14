7b56f8b2f9dde0c93aa8e76932bbfe2c35b1c6e2
Lauren <Laurenelee88@gmail.com>
2017-11-14 11:44:59 -0800

    score method

--- scrabble.js
+++ scrabble.js
@@ -1,28 +1,74 @@
 'use strict';
 var prompt = require('prompt');
-prompt.start();
-prompt.get('word', Scrabble.score)
+const LETTER_VALUES = {
+  a: 1,
+  b: 3,
+  c: 3,
+  d: 2,
+  e: 1,
+  f: 4,
+  g: 2,
+  h: 4,
+  i: 1,
+  j: 8,
+  k: 5,
+  l: 1,
+  m: 3,
+  n: 1,
+  o: 1,
+  p: 3,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 1,
+  v: 4,
+  w: 4,
+  x: 8,
+  y: 4,
+  z: 10
+};
 
 
 const Scrabble = {
 
   score: function(word) {
+    // TODO: check length > 0 and < 7
+    word = word.toLowerCase();
     let score = 0;
     // turn word into an array of characters
+    word = word.split("");
     // forEach loop
-    // ruby way: 
-    // score = 0
-    // word.chars.each do |char|
-    //   score += LETTER_VALUES[char.downcase]
-    // end
-    // score += 50 if word.length == 7
-    // return score
+    word.forEach(function(char) {
+      if (!(char in LETTER_VALUES)) {
+        throw char 'is not a character'
+      }
+      score += LETTER_VALUES[char];
+    }
 
-  }
+    if (word.length == 7) {
+      score += 50;
+    }
 
-  // TODO: add the highestScoreFrom method
+    return score;
+  );
+
+
+  // my old ruby way:
+  // score = 0
+  // word.chars.each do |char|
+  //   score += LETTER_VALUES[char.downcase]
+  // end
+  // score += 50 if word.length == 7
+  // return score
+
+}
+
+// TODO: add the highestScoreFrom method
 
 };
+prompt.start();
+prompt.get('word', Scrabble.score)
 
 
 // wave 2:
@@ -32,31 +78,3 @@ Scrabble.Player = class {
 
 module.exports = Scrabble;
 // where to store the letter value
-const LETTER_VALUES = {
-  'a' => 1,
-  'b' => 3,
-  'c' => 3,
-  'd' => 2,
-  'e' => 1,
-  'f' => 4,
-  'g' => 2,
-  'h' => 4,
-  'i' => 1,
-  'j' => 8,
-  'k' => 5,
-  'l' => 1,
-  'm' => 3,
-  'n' => 1,
-  'o' => 1,
-  'p' => 3,
-  'q' => 10,
-  'r' => 1,
-  's' => 1,
-  't' => 1,
-  'u' => 1,
-  'v' => 4,
-  'w' => 4,
-  'x' => 8,
-  'y' => 4,
-  'z' => 10
-}
