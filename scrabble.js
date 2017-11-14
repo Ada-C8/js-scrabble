13aa346cf75f0ddeec16482a490ff9117503c675
Diane Lenertz <lenertzd@grinnell.edu>
2017-11-14 15:12:27 -0800

    initial setup and first stab at score function

--- scrabble.js
+++ scrabble.js
@@ -1,10 +1,21 @@
 const Scrabble = {
+  scoring: { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1,
+      R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3,
+      M: 3, P:3, F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5,
+      J: 8, X: 8, Q: 10, Z: 10
+    },
   score: function(word) {
+    let total = 0
+    for(let letter of word) {
+    // word.forEach(function(letter) {
+      total += this.scoring[letter]
+    }
+    return total
     // TODO: implement score
-  }
+  },
 
   // TODO: add the highestScoreFrom method
-
+// static method
 };
 
 Scrabble.Player = class {
@@ -12,3 +23,6 @@ Scrabble.Player = class {
 };
 
 module.exports = Scrabble;
+
+console.log(Scrabble.score('THIS'))
+// Scrabble.scoring.A
