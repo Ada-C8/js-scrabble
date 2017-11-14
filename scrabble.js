dc32db54f72f05baccee6c0abc32981367661a9a
Amy Cash <amymcash@gmail.com>
2017-11-14 15:38:41 -0800

    Write scoring function

--- scrabble.js
+++ scrabble.js
@@ -1,11 +1,24 @@
+const letterPoints = {
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10
+};
+
+
+let total = 0;
+
+
 const Scrabble = {
   score: function(word) {
-    // TODO: implement score
-  }
+
+    for(let i = 0; i < word.length; i++){
+      total += letterPoints[word[i]];
+    } // end for loop
+        return total;
+
+  } //end function word
 
   // TODO: add the highestScoreFrom method
 
-};
+}; //end const Scrabble
 
 Scrabble.Player = class {
   // TODO: implement the Player class
