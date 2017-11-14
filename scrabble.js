8c23ffa1cbc302b3471bc1c1a54ed34cd40fb063
Gale Harrington <gale.harrington19@gmail.com>
2017-11-14 15:47:17 -0800

    Fix style issues per airbnb

--- scrabble.js
+++ scrabble.js
@@ -4,7 +4,10 @@ const Scrabble = {
   score: function(word) {
     let score = 0;
     const LETTER_TO_VALUE = {
-      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10,
+      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1,
+      R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3,
+      M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5, J: 8, X: 8, Q: 10, Z: 10,
     };
     const ALPHA = /[a-zA-Z]/
     function ArgumentError(message) {
@@ -16,7 +19,7 @@ const Scrabble = {
     }
     word.split('').forEach( function (letter) {
       if (letter.match(ALPHA)) {
-        score += parseInt(LETTER_TO_VALUE[letter.toUpperCase()]);
+        score += parseInt(LETTER_TO_VALUE[letter.toUpperCase()], 10);
       } else {
         throw new ArgumentError('Invalid characters detected');
       }
@@ -27,7 +30,7 @@ const Scrabble = {
     return score;
   }
   // TODO: add the highestScoreFrom method
-}
+};
 
 Scrabble.Player = class {
   // TODO: implement the Player class
