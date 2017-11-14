bc8a7a7e1d24617842f41eab480571ad161e3fae
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-14 15:04:56 -0800

    starting project and doing initial work on scores method

--- scrabble.js
+++ scrabble.js
@@ -1,6 +1,18 @@
+const scores = {
+  A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4, I:1, J:8, K:5, L:1, M:3, N:1, O:1, P:3, Q:10, R:1, S:1, T:1, U:1, V:4, W:4, X:8, Y:4, Z:10
+};
+
+// TODO: add error handling and raising of exceptions
+
 const Scrabble = {
   score: function(word) {
-    // TODO: implement score
+    // toUpperCase(word);
+    const scoreTotal = 0;
+
+    // console.log()
+    for (let letter of word) {
+      console.log(scores[letter.toUpperCase()]);
+    }
   }
 
   // TODO: add the highestScoreFrom method
@@ -12,3 +24,5 @@ Scrabble.Player = class {
 };
 
 module.exports = Scrabble;
+
+Scrabble.score('dog')
