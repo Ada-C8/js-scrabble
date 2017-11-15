159bed18c9984aec4847c56989a5266380a0b8e0
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-14 17:06:41 -0800

    Created Map for letters and score pairs

--- scrabble.js
+++ scrabble.js
@@ -1,7 +1,21 @@
+/* eslint-disable */
 const Scrabble = {
-  score: function(word) {
+  score: function score(word) {
     // TODO: implement score
-  }
+    if (word.length > 7 || word.length < 1){
+      throw new Error(`${word} has to have 1 to 7 letters`);
+    }
+
+    const uppercaseWord = word.toUpperCase();
+    const letterMap = new Map([
+      [1, ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']],
+      [2, ['D', 'G']],
+      [3, ['B', 'C', 'M', 'P']],
+      [4, ['F', 'H', 'V', 'W', 'Y']],
+      [5, ['K']],
+      [8, ['J', 'X']],
+      [10, ['Q', 'Z']],
+    ]);
 
   // TODO: add the highestScoreFrom method
 
