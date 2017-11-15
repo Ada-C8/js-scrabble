fdd663ce52b2f7cb14078b9ccca6192ba81188f6
Amy Cash <amymcash@gmail.com>
2017-11-14 18:55:30 -0800

    Empty array, nonArray and 1 item array work

--- scrabble.js
+++ scrabble.js
@@ -16,7 +16,7 @@ const Scrabble = {
       throw new UserException("You must enter a word to be scored");
     } else if (word.match(/[^a-z]+/i)) {
       throw new UserException("Word can only contain letters");
-    }
+    } //exception tests
 
     let total = 0; //set initial total
 
@@ -28,21 +28,33 @@ const Scrabble = {
 
     if(word.length === 7){
       total += 50;
-    }
+    } //if length
 
     return total;
 
   }, //end function word
 
   highestScoreFrom: function(words) {
+
+    if(words.length === 0) {
+      throw new UserException("Array is Empty");
+    } else if (typeof words != 'object') {
+      throw new UserException("You must enter an array");
+    } //exceptions
+
     if(words.length === 1) {
       return words[0];
-    }
-  }
-  // highestScoreFrom: function(words) {
-  //   return words
-  // }
+    } else if(words.length >= 2) {
+      let max = Scrabble.score(words[0]);
 
+      words.forEach(function (word) {
+        if (Scrabble.score(word) > max){
+          max = Scrabble.score(word);
+        } //if
+      }); //forEach
+      return max
+    } //else if
+  }
 }; //end const Scrabble
 
 Scrabble.Player = class {
