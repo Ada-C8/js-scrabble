d688e40b5e29f70b1e417a6e4586e524d57bf506
Amy Cash <amymcash@gmail.com>
2017-11-14 16:39:40 -0800

    Add highest score from 1 word array

--- scrabble.js
+++ scrabble.js
@@ -7,16 +7,14 @@ const letterPoints = {
   A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10
 };
 
-
 const Scrabble = {
   score: function(word) {
 
     if(word.length > 7) {
       throw new UserException("Word is Too Long");
     } else if (word.length === 0) {
-      throw new UserException("You must enter a word to be scored"
-    );
-  } else if (word.match(/[^a-z]+/i)) {
+      throw new UserException("You must enter a word to be scored");
+    } else if (word.match(/[^a-z]+/i)) {
       throw new UserException("Word can only contain letters");
     }
 
@@ -34,9 +32,16 @@ const Scrabble = {
 
     return total;
 
-  } //end function word
+  }, //end function word
 
-  // TODO: add the highestScoreFrom method
+  highestScoreFrom: function(words) {
+    if(words.length === 1) {
+      return words[0];
+    }
+  }
+  // highestScoreFrom: function(words) {
+  //   return words
+  // }
 
 }; //end const Scrabble
 
