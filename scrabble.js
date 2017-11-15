1034b1f9cf64a7b75e678ba027a38404e64293cf
Laura Robertson <mslaura.mae@gmail.com>
2017-11-15 13:26:23 -0800

    Linted to AirBnb standards

--- scrabble.js
+++ scrabble.js
@@ -1,81 +1,93 @@
-/* eslint-disable */
-
 const Scrabble = {
-  score: function(word) {
+  score(word) {
     let score = 0;
     const ALPHA = /^[a-z]+$/i;
     const SCORECHART = {
-      a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8,
-      k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1,
-      u: 1, v: 4, w: 4, x: 8, y: 4, z: 10,
+      a: 1,
+      b: 3,
+      c: 3,
+      d: 2,
+      e: 1,
+      f: 4,
+      g: 2,
+      h: 4,
+      i: 1,
+      j: 8,
+      k: 5,
+      l: 1,
+      m: 3,
+      n: 1,
+      o: 1,
+      p: 3,
+      q: 10,
+      r: 1,
+      s: 1,
+      t: 1,
+      u: 1,
+      v: 4,
+      w: 4,
+      x: 8,
+      y: 4,
+      z: 10,
     };
 
     if (typeof word === 'string' && word.length > 0 && word.length < 8) {
       if (word.length === 7) {
         score += 50;
       }
-
-      for (i = 0; i < word.length; i++) {
-        let letter = word[i]
+      for (let i = 0; i < word.length; i += 1) {
+        const letter = word[i];
         if (letter.match(ALPHA)) {
-          score += parseInt(SCORECHART[letter.toLowerCase()]);
-        }
-        else {
+          score += parseInt(SCORECHART[letter.toLowerCase()], 10);
+        } else {
           throw new Error('Characters only');
         }
       }
-      return score;
     } else {
       throw new Error('Not a valid word');
     }
+    return score;
   },
 
-  highestScoreFrom: function(wordArray) {
+  highestScoreFrom(wordArray) {
     if (wordArray.length === 0) {
       throw new Error('There are no words to score');
-    }
-    else if (wordArray.length === 1) {
+    } else if (wordArray.length === 1) {
       return wordArray[0];
-    }
-    else {
-      let highestWords = this.highestScores(wordArray);
-
+    } else {
+      const highestWords = this.highestScores(wordArray);
       return highestWords.length === 1 ? highestWords[0] : this.tiebreaker(highestWords);
     }
   },
 
-  highestScores: function(wordArray) {
-    let wordScores = wordArray.map((word) => {
-      return this.score(word);
-    });
-
+  highestScores(wordArray) {
+    const wordScores = wordArray.map(word => this.score(word));
     const highestScore = Math.max(...wordScores);
-    let highestWords = [];
+    const highestWords = [];
 
     wordArray.forEach((word) => {
-      if (this.score(word) == highestScore) {
+      if (this.score(word) === highestScore) {
         highestWords.push(word);
       }
     });
     return highestWords;
   },
 
-  tiebreaker: function(highestWords) {
+  tiebreaker(highestWords) {
     let highestWord = '';
     let lengthCompare = 8;
 
-    for (i = 0; i < highestWords.length; i++) {
+    for (let i = 0; i < highestWords.length; i += 1) {
       if (highestWords[i].length === 7) {
         highestWord = highestWords[i];
         break;
-      }
-      else if (highestWords[i].length < lengthCompare) {
+      } else if (highestWords[i].length < lengthCompare) {
         highestWord = highestWords[i];
         lengthCompare = highestWords[i].length;
       }
     }
     return highestWord;
-  }
+  },
 
 };
 
@@ -97,11 +109,9 @@ Scrabble.Player = class {
     word.toLowerCase();
     if (word.length > 8) {
       throw new Error('You cannot play a word longer than 7 letters');
-    }
-    else if (this.plays.includes(word)) {
+    } else if (this.plays.includes(word)) {
       throw new Error('You have alreayd played this word');
-    }
-    else {
+    } else {
       this.plays.push(word);
       this.score += Scrabble.score(word);
     }
@@ -123,7 +133,6 @@ Scrabble.Player = class {
   highestWordScore() {
     return Scrabble.score(Scrabble.highestScoreFrom(this.plays));
   }
-
 };
 
 module.exports = Scrabble;
