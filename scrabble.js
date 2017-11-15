b7b603bc7f601b687697ebec3546aa828d851dc9
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-14 22:18:04 -0800

    pass first four tests, fleshing out score function

--- scrabble.js
+++ scrabble.js
@@ -1,28 +1,31 @@
 const Scrabble = {
-  const POINTS = {
-    A: 1, B: 3, C: 3, D: 2, E: 1, F: 4,
-    G: 2, H: 4, I: 1, J: 8, K: 5, L: 1,
-    M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1,
-    S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
-    Y: 4, Z: 10
-  }
-  
   score: function(word) {
-    this.word = word;
 
-    let letters = word.upcase.split('');
+    const POINTS = {
+      A: 1, B: 3, C: 3, D: 2, E: 1, F: 4,
+      G: 2, H: 4, I: 1, J: 8, K: 5, L: 1,
+      M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1,
+      S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+      Y: 4, Z: 10
+    };
+
+    let letters = word.toUpperCase().split('');
     let totalScore = 0;
 
     letters.forEach(function(letter) {
+      if (!(letter in POINTS)) {
+        throw 'is not a valid char'
+      };
       totalScore += POINTS[letter];
     });
-  }
+    if (word.length === 7) {
+      totalScore += 50;
+    };
+    return totalScore;
+  },
 
   highestScoreFrom: function(arrayOfWords) {
-    let (word in arrayOfWords) {
-      if (word.length === 7) {
-        totalScore += 50;
-      };
+    for (word in arrayOfWords) {
     }
   }
   // TODO: add the highestScoreFrom method
