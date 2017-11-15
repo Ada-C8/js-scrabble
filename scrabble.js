66df4f0aee9ff5af8c9cc7806010107fd13e461c
Jan Edrozo <jedrozo@gmail.com>
2017-11-14 17:05:03 -0800

    Updated Scrabble#score method to pass tests to score simple words

--- scrabble.js
+++ scrabble.js
@@ -1,9 +1,57 @@
+/* eslint-disable */
+
 const Scrabble = {
   score: function(word) {
-    // TODO: implement score
-  }
+    const letterValues = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    }
 
-  // TODO: add the highestScoreFrom method
+    const splitChars = function splitChars(str) {
+      const chars = [];
+      for (let i = 0; i < str.length; i++) {
+        chars.push(str.charAt(i))
+      };
+      return chars;
+    };
+
+    const totalScore = function totalScore(array) {
+      let total = 0;
+      array.forEach((char) => {
+        total = total + letterValues[char]
+      });
+      return total;
+    }
+    const uppercaseWord = word.toUpperCase();
+
+    const characters = splitChars(uppercaseWord);
+
+    return totalScore(characters);
+  }
 
 };
 
