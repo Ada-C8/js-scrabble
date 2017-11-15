1dfc117b21f171803ed2e54cc59fa7c8ef8baa67
Tanja Stroble <plusdeltah@gmail.com>
2017-11-14 16:06:25 -0800

    ammends score to include letterScore hash inside

--- scrabble.js
+++ scrabble.js
@@ -1,15 +1,15 @@
 
 const Scrabble = {
-  let letterScores = {
-      A: 1, E: 1, I: 1, O: 1, U: 1,
-      L: 1, N: 1, R: 1, S: 1, T: 1,
-      D: 2, G: 2, B: 3, C: 3, M: 3,
-      P: 3, F: 4, H: 4, V: 4, W: 4,
-      Y: 4, K: 5, J: 8, X: 8, Q: 10,
-      Z: 10
-    };
-
   score: function(word) {
+
+    let letterScores = {
+        A: 1, E: 1, I: 1, O: 1, U: 1,
+        L: 1, N: 1, R: 1, S: 1, T: 1,
+        D: 2, G: 2, B: 3, C: 3, M: 3,
+        P: 3, F: 4, H: 4, V: 4, W: 4,
+        Y: 4, K: 5, J: 8, X: 8, Q: 10,
+        Z: 10
+      };
 
     let letters = word.toUpperCase().split("");
     let wordScore = 0;
@@ -18,27 +18,23 @@ const Scrabble = {
       if (letterScores[letter] !== null) {
         wordScore += letterScores[letter];
       }
-      if word.length === 7{
+      if (word.length === 7){
         wordScore += 50;
       }
     }
     return wordScore;
+  },
+
+  highestScoreFrom: function(arrayOfWords){
+
   }
 
-  // TODO: add the highestScoreFrom method
+}; // do not remove me, i end the const !!!
+
+
 
 Scrabble.Player = class {
   // TODO: implement the Player class
 };
 
 module.exports = Scrabble;
-
-
-letterScores = {
-    A: 1, E: 1, I: 1, O: 1, U: 1,
-    L: 1, N: 1, R: 1, S: 1, T: 1,
-    D: 2, G: 2, B: 3, C: 3, M: 3,
-    P: 3, F: 4, H: 4, V: 4, W: 4,
-    Y: 4, K: 5, J: 8, X: 8, Q: 10,
-    Z: 10
-  };
