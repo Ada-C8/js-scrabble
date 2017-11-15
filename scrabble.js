70c007b2db60c1c9cf498e4aab17182241453512
Julia <julia.meier@gmail.com>
2017-11-15 13:19:28 -0800

    added if statement to the score method to screen for empty screens and invalid characters

--- scrabble.js
+++ scrabble.js
@@ -1,70 +1,101 @@
-const scoreChart = {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-    2: ['D','G'],
-    3: ['B', 'C', 'M', 'P'],
-    4: ['F', 'H', 'V', 'W', 'Y'],
-    5: ['K'],
-    8: ['J', 'X'],
-    10: ['Q','Z'],
-  };
+//Julia Meier - JS Scrabble
 
-const letterValue = function letterValue(letter) {
-  let value = 0;
-  const points = Object.keys(scoreChart);
-  points.forEach(function(point) {
-    if (scoreChart[point].includes(letter)) {
-      value = point;
-    };
-  });
-  //console.log(letter);
-  return Number(value);
-};
-
-//TESTING letterValue
-let a = letterValue('I');
-console.log(a);
-
+//EXCEPTIONS:
+function UserException(message) {
+   this.message = message;
+   this.name = 'UserException';
+}
+//throw new UserException('InvalidMonthNo');
 
 const Scrabble = {
+  scoreChart: {
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+      2: ['D','G'],
+      3: ['B', 'C', 'M', 'P'],
+      4: ['F', 'H', 'V', 'W', 'Y'],
+      5: ['K'],
+      8: ['J', 'X'],
+      10: ['Q','Z'],
+    },
+
+  letterValue: function(letter) {
+    let value = 0;
+    const points = Object.keys(Scrabble.scoreChart);
+    points.forEach(function(point) {
+      if (Scrabble.scoreChart[point].includes(letter)) {
+        value = point;
+      } 
+    });
+    //console.log(letter);
+    return Number(value);
+  },
 
   score: function(word) {
     let total = 0;
+    if (null == word || word === "" || /[^a-zA-Z]/.test(word)) {
+      throw 'You did not enter a valid word (empty string or non alphabetic characters)';
+    }
     for (let i = 0, len = word.length; i < len; i++) {
-      value = letterValue(word[i].toUpperCase());
-      total += value;
+      let value = Scrabble.letterValue(word[i].toUpperCase());
+      total += value
+    }
+    if (word.length === 7) {
+      total += 50;
     }
     return total;
   },
 
   highestScoreFrom: function(words) {
     let values = words.map(function(word) {
-      console.log(Scrabble.score(word));
+      //console.log(Scrabble.score(word));
       return Scrabble.score(word);
     });
-  
-    let i = values.indexOf(Math.max(...values));
-    console.log(`index of highest scoring word: ${i}`);
-    console.log(words[i]);
-    return words[i];
+    let maxValue = Math.max.apply(null, values);
+    let numberOfHighestScore = values.filter(item => item == maxValue).length;
+    console.log(`number of high score: ${numberOfHighestScore}`);
+    let winningWord = null;
+    if (numberOfHighestScore === 1) {
+      let i = values.indexOf(maxValue);
+      // let i = values.indexOf(Math.max(...values));
+      console.log(`index of highest scoring word: ${i}`);
+      console.log(words[i]);
+      winningWord = words[i];
+    } else {
+        let bestWords = [];
+        let bestWordsLengths = [];
+        for (let i = 0; i < values.length; i+=1) {
+          if (values[i] === maxValue) {
+            bestWords.push(words[i]);
+            bestWordsLengths.push(words[i].length);
+          }
+        }
+        const minLength = Math.min.apply(null,bestWordsLengths);
+        let numberOfWordsAtMinLength = bestWordsLengths.filter(item => item == minLength).length;
+        let i = bestWordsLengths.indexOf(minLength);
+        winningWord = bestWords[i];
+    }
+    return winningWord;
   }
-
 };
+
+
+
+//TESTING letterValue
+//let a = Scrabble.letterValue('I');
+//console.log(a);
 
 //TESTING score
 let answer = Scrabble.score('Hello');
-console.log(answer);
+//console.log(answer);
 
 //TESTING highestScoreFrom
-words = ['hi', 'hello', 'howdy'];
-winning_word = Scrabble.highestScoreFrom(words);
-console.log(winning_word);
-
-  // TODO: add the highestScoreFrom method
-
+//words = ['hi', 'hello', 'howdy', 'howddd', 'abcdefg', 'gfedcba'];
+//winning_word = Scrabble.highestScoreFrom(words);
+//console.log(`Winning word: ${winning_word}`);
 
 
 // Scrabble.Player = class {
   // TODO: implement the Player class
 // };
 
-// module.exports = Scrabble;
+module.exports = Scrabble;
