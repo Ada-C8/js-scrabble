961db58ad293e39fcdef17cfac2032dbc22a8f45
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-15 11:13:09 -0800

    completes score function

--- scrabble.js
+++ scrabble.js
@@ -1,9 +1,105 @@
+// const values = {
+//   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+//   2: ['D', 'G'],
+//   3: ['B', 'C', 'M', 'P'],
+//   4: ['F', 'H', 'V', 'W', 'Y'],
+//   5: ['K'],
+//   8: ['J', 'X'],
+//   10: ['Q', 'Z']
+// };
+
+// const values = {
+// A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10
+// };
+
+// =======================================================
+
 const Scrabble = {
+
   score: function(word) {
-    // TODO: implement score
+    let score = 0;
+
+    if (/^[a-zA-Z]+$/.test(word)) {
+    } else {
+      throw 'invalid word';
+    }
+
+    if (word.length > 7 || word.length === 0) {
+      throw 'invalid word';
+    }
+
+    if (word.length === 7) {
+      score += 50;
+    } else {
+      score = 0;
+    }
+
+    const upperCase = word.toUpperCase();
+    let letterArray = upperCase.split('');
+
+    letterArray.forEach(function (letter){
+
+      switch (letter) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'L':
+        case 'N':
+        case 'R':
+        case 'S':
+        case 'T':
+          score += 1;
+          break;
+
+        case 'D':
+        case 'G':
+          score += 2;
+          break;
+
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P':
+          score += 3;
+          break;
+
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y':
+          score += 4;
+          break;
+
+        case 'K':
+          score += 5;
+          break;
+
+        case 'J':
+        case 'X':
+          score += 8;
+          break;
+
+        case 'Q':
+        case 'Z':
+          score += 10;
+          break;
+
+        default:
+          throw 'invalid letter';
+      }
+    });
+    return score;
   }
 
-  // TODO: add the highestScoreFrom method
+
+  highestScoreFrom: function(words_array) {
+
+  };
+
+
 
 };
 
@@ -12,3 +108,8 @@ Scrabble.Player = class {
 };
 
 module.exports = Scrabble;
+
+
+
+
+// console.log(Scrabble.score('apple'));
