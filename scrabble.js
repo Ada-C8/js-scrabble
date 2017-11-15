1bef12ad32db5d041ad326dd09c1c2be5ab4f526
murog <guillermina.muro@gmail.com>
2017-11-14 16:08:21 -0800

    passing tests for score describe block

--- scrabble.js
+++ scrabble.js
@@ -1,6 +1,45 @@
+/* eslint-disable */
+
 const Scrabble = {
-  score: function(word) {
-    // TODO: implement score
+  score: function score(word) {
+    const letters = {
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+      2: ['D', 'G'],
+      3: ['B', 'C', 'M', 'P'],
+      4: ['F', 'H', 'V', 'W', 'Y'],
+      5: ['K'],
+      8: ['J', 'X'],
+      10: ['Q', 'Z']
+    };
+    const keys = Object.keys(letters);
+    console.log(keys);
+    let totalScore = 0;
+    console.log('score starting');
+    if (/^[a-zA-Z]+$/.test(word)) {
+
+    }
+    if (((typeof word === 'string')) && (/^[a-zA-Z]+$/.test(word)) && (word.length < 8)) {
+      console.log('im a string');
+      const chars = word.toUpperCase().match(/\S/g);
+      console.log(chars);
+      chars.forEach(function(element) {
+        keys.forEach(function(num) {
+          let numLetters = letters[num];
+          console.log(numLetters);
+          if (numLetters.includes(element)) {
+            console.log(`${num}`);
+            totalScore += parseFloat(num);
+          }})
+      }
+    )
+    if (word.length === 7) {
+      totalScore += 50;
+    }
+  } else {
+    throw 'Invalid input';
+  }
+    return totalScore;
+
   }
 
   // TODO: add the highestScoreFrom method
