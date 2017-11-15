1266c1354afe77756b5b52fe349d118cef82eebf
Bennett Rahn <rahnibennett@gmail.com>
2017-11-14 17:45:34 -0800

    Player class

--- scrabble.js
+++ scrabble.js
@@ -7,19 +7,26 @@ const scores = {
 };
 
 const UserException = function UserException(message) {
-  this.message = message
-  this.name = 'UserException'
+  this.message = message;
+  this.name = 'UserException';
 }
 
+// const invalidWord = function invalidWord(word) {
+//   if ( word.length > 7 || word.length === 0 || !/^[a-zA-Z]+$/.test(word) ) {
+//     throw new UserException('invalid word, please try again');
+//   }
+// }
+
 const Scrabble = {
-  // scores: {
-  //   A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4, I:1, J:8, K:5, L:1, M:3, N:1, O:1, P:3, Q:10, R:1, S:1, T:1, U:1, V:4, W:4, X:8, Y:4, Z:10
-  // },
+  invalidWord(word) {
+    if ( !/^[a-z]{1,7}$/i.test(word) ) {
+      throw new UserException('invalid word, please try again');
+    }
+  },
 
   score(word) {
-    if (word.length > 7) {
-      throw new UserException('too many letters')
-    }
+    // console.log(this.invalidWord);
+    this.invalidWord(word)
 
     let chars = word.toUpperCase().split('');
     let score = 0;
@@ -36,20 +43,26 @@ const Scrabble = {
   },
 
   highestScoreFrom(arrayOfWords) {
+    if ( arrayOfWords.length === 0 ) {
+      throw new UserException('no words given');
+    }
+
     let highestScoringWord = arrayOfWords[0]
+
     arrayOfWords.forEach( function(word) {
+      // console.log(`${this}`); // returns the .forEach function [object global]
       let highScore = Scrabble.score(highestScoringWord);
       let score = Scrabble.score(word);
 
       if ( score > highScore ) {
-        highestScoringWord = word
+        highestScoringWord = word;
       } else if (score === highScore) {
         if (word.length === 7){
-          highestScoringWord = word
+          highestScoringWord = word;
         } else if (highestScoringWord.length === 7) {
 
         } else if (word.length < highestScoringWord.length) {
-          highestScoringWord = word
+          highestScoringWord = word;
         }
       }
     });
@@ -58,7 +71,46 @@ const Scrabble = {
 };
 
 Scrabble.Player = class {
-  // TODO: implement the Player class
+  constructor(name) {
+    if (!name) {
+      throw new UserException('Player must have a name');
+    }
+
+    this.name = name
+    this.plays = []
+    this._totalScore = 0
+  }
+
+  totalScore() {
+    return this._totalScore;
+  }
+
+  play(word) {
+    if (this.hasWon()){
+      return false
+    }
+    Scrabble.invalidWord(word)
+    this._totalScore += Scrabble.score(word)
+    this.plays.push(word)
+    return true
+  }
+
+  hasWon() {
+    if (this._totalScore < 100) {
+      return false
+    }
+    return true
+  }
+
+  highestScoringWord() {
+    return Scrabble.highestScoreFrom(this.plays)
+  }
+
+  highestWordScore() {
+    return Scrabble.score(this.highestScoringWord())
+  }
 };
 
 module.exports = Scrabble;
+
+// Scrabble.highestScoreFrom(['cat'])
