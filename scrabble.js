e3736688d1022141db1b9ff62963e730678c3c5e
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-14 16:46:23 -0800

    highestScoreWord does basic function of picking the hgihest scoring word from the array

--- scrabble.js
+++ scrabble.js
@@ -9,16 +9,17 @@ const Scrabble = {
 /* score(word): returns the total score value for the given word. The word is input as a string (case insensitive). The chart below shows the point value for a given letter. */
   score: function score(word) {
     const scoreChart = {
-       'A': 1, 'E': 1, 'I': 1,
-       'O': 1, 'U': 1, 'L': 1,
-       'N': 1, 'R': 1, 'S': 1,
-       'T': 1, 'D': 2, 'G': 2,
-       'B': 3, 'C': 3, 'M': 3,
-       'P': 3, 'F': 4, 'H': 4,
-       'V': 4, 'W': 4, 'Y': 4,
-       'K': 5, 'J': 8, 'X': 8,
-       'Q': 10, 'Z': 10
+      'A': 1, 'E': 1, 'I': 1,
+      'O': 1, 'U': 1, 'L': 1,
+      'N': 1, 'R': 1, 'S': 1,
+      'T': 1, 'D': 2, 'G': 2,
+      'B': 3, 'C': 3, 'M': 3,
+      'P': 3, 'F': 4, 'H': 4,
+      'V': 4, 'W': 4, 'Y': 4,
+      'K': 5, 'J': 8, 'X': 8,
+      'Q': 10, 'Z': 10
      };
+
     let totalScore = 0;
     // so that we can look up letter in hash
     word = word.toUpperCase()
@@ -39,7 +40,32 @@ const Scrabble = {
     return totalScore;
   },
 
-  // TODO: add the highestScoreFrom method
+  /*
+  highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
+    Note that it’s better to use fewer tiles, so if the top score is tied between multiple words, pick the one with the fewest letters.
+    Note that there is a bonus (50 points) for using all seven letters. If the top score is tied between multiple words and one used all seven letters, choose the one with seven letters over the one with fewer tiles.
+    If the there are multiple words that are the same score and same length, pick the first one in supplied list.
+  */
+
+
+
+  highestScoreFrom: function highestWordFrom(words) {
+    // throw an expection if there if passed an empty array
+    if (words.length < 1) {
+      throw new UserException('You cannot score an empty array');
+    }
+
+    let maxScore = 0;
+    let maxWord;
+    words.forEach( function (word) {
+      let wordScore = Scrabble.score(word);
+      if (wordScore > maxScore) {
+        maxScore = wordScore
+        maxWord = word;
+      } // if
+    }); // forEach
+    return maxWord
+  }, // highestWordFrom
 
 };
 
