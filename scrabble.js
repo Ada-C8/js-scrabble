33f5eeb175ccab5daa034cb6ba099228afb53ed1
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-16 11:50:21 -0800

    updated conditional in scrabble object by removing the extra else statement.

--- scrabble.js
+++ scrabble.js
@@ -8,10 +8,6 @@
 //   10: ['Q', 'Z']
 // };
 
-// const values = {
-// A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10
-// };
-
 // =======================================================
 
 const Scrabble = {
@@ -19,8 +15,7 @@ const Scrabble = {
   score(word) {
     let score = 0;
 
-    if (/^[a-zA-Z]+$/.test(word)) {
-    } else {
+    if (!word.match(/^[a-zA-Z]+$/)) {
       throw new Error('invalid word');
     }
 
@@ -129,7 +124,6 @@ Scrabble.Player = class {
   }
 
   play(word) {
-
     if (this.hasWon() === true) {
       return false;
     }
